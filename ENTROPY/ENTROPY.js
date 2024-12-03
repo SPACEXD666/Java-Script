e7a2a1b2e48f71e76b54a268e805f5aa0ee9c899
SPACEXD666 <matveyshtol1337007228@gmail.com>
2024-12-03 23:00:36 +0500

    Add files via upload

---
+++ ENTROPY/ENTROPY.js
@@ -0,0 +1,32 @@
+﻿const fs = require("fs");
+
+function Shanon_ent(text) {
+    let freq = {};
+    let H_x = 0;
+
+    for (let i = 0; i < text.length; i++) {
+        const char = text[i];
+        freq[char] = (freq[char] || 0) + 1;
+    }
+
+    for (let char in freq) {
+        const Pi_value = freq[char] / text.length;
+        H_x -= Pi_value * Math.log2(Pi_value);
+    }
+
+    return H_x;
+}
+
+const filePath = process.argv[2];
+
+if (!filePath) {
+    console.log("Ошибка: укажите путь к файлу.");
+    process.exit(1);
+}
+
+try {
+    let text = fs.readFileSync(filePath, 'utf-8');
+    console.log("Энтропия Шеннона: " + Shanon_ent(text));
+} catch (err) {
+    console.error("Ошибка при чтении файла:", err.message);
+}
