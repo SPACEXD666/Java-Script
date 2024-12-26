0ce0070a08dc99fc704ff54adb8f86c1a917ea2b
SPACEXD666 <matveyshtol1337007228@gmail.com>
2024-12-26 07:17:53 +0500

    Create DIJKSTRA.js

---
+++ DIJKSTRA/DIJKSTRA.js
@@ -0,0 +1,125 @@
+const readline = require('readline');
+
+// Создаем интерфейс для ввода/вывода
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Проверка корректности входного выражения
+function validateExpression(expression) {
+    const validCharacters = /^[\d+\-*/^().\s]+$/;
+    if (!validCharacters.test(expression)) {
+        throw new Error('Выражение содержит недопустимые символы.');
+    }
+
+    let balance = 0; // Баланс скобок
+    const tokens = expression.match(/\d+(\.\d+)?|[+\-*/^()]|\s+/g);
+
+    for (const token of tokens) {
+        if (token.trim() === '') continue;
+        if (token === '(') balance++;
+        if (token === ')') balance--;
+        if (balance < 0) throw new Error('Несбалансированные скобки.');
+    }
+
+    if (balance !== 0) throw new Error('Несбалансированные скобки.');
+}
+
+// Преобразование инфиксной записи в постфиксную
+function infixToPostfix(expression) {
+    const precedence = {
+        '+': 1,
+        '-': 1,
+        '*': 2,
+        '/': 2,
+        '^': 3
+    };
+
+    const isRightAssociative = { '^': true };
+
+    // Удаляем лишние пробелы
+    expression = expression.replace(/\s+/g, '');
+
+    // Обработка унарного минуса
+    expression = expression.replace(/(^|[(])-/g, '$10-');
+
+    const stack = [];
+    const postfix = [];
+    const tokens = expression.match(/\d+(\.\d+)?|[+\-*/^()]|[^\s]/g); // Числа, операторы и скобки
+
+    for (const token of tokens) {
+        if (!isNaN(token)) {
+            postfix.push(token);
+        } else if (token === '(') {
+            stack.push(token);
+        } else if (token === ')') {
+            while (stack.length && stack[stack.length - 1] !== '(') {
+                postfix.push(stack.pop());
+            }
+            stack.pop();
+        } else {
+            while (
+                stack.length &&
+                precedence[stack[stack.length - 1]] !== undefined &&
+                (
+                    (isRightAssociative[token] && precedence[token] < precedence[stack[stack.length - 1]]) ||
+                    (!isRightAssociative[token] && precedence[token] <= precedence[stack[stack.length - 1]])
+                )
+            ) {
+                postfix.push(stack.pop());
+            }
+            stack.push(token);
+        }
+    }
+
+    // Выталкиваем оставшиеся операторы из стека
+    while (stack.length) {
+        postfix.push(stack.pop());
+    }
+
+    return postfix.join(' ');
+}
+
+// Вычисление результата постфиксной записи
+function evaluatePostfix(postfixExpression) {
+    const stack = [];
+    const tokens = postfixExpression.split(' ');
+
+    for (const token of tokens) {
+        if (!isNaN(token)) {
+            stack.push(Number(token));
+        } else {
+            const b = stack.pop();
+            const a = stack.pop();
+
+            switch (token) {
+                case '+':
+                    stack.push(a + b);
+                    break;
+                case '-':
+                    stack.push(a - b);
+                    break;
+                case '*':
+                    stack.push(a * b);
+                    break;
+                case '/':
+                    stack.push(a / b);
+                    break;
+                case '^':
+                    stack.push(Math.pow(a, b));
+                    break;
+                default:
+                    throw new Error(Неизвестный оператор: ${token});
+            }
+        }
+    }
+
+    return stack[0];
+}
+
+rl.question('Введите инфиксное выражение: ', (infixExpression) => {
+    try {
+        validateExpression(infixExpression);
+        const postfixExpression = infixToPostfix(infixExpression);
+        console.log('Постфиксная запись:', postfixExpression);
